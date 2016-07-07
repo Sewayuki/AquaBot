71189d70be013a6c97a0bf634be1793c8c19111a
Sewayuki <sewayuki@gmail.com>
2016-07-07 05:28:59 +0300

    BnsModule create

---
+++ modules/BnsModule.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class BnsModule
+{
+	function __construct()
+	{
+		//
+	}
+
+	public function getBnsBaseItem()
+	{
+		//
+	}
+
+	public function searchBnsBaseItem()
+	{
+		//
+	}
+
+	public function getBnsProfileInfo()
+	{
+		//
+	}
+}
